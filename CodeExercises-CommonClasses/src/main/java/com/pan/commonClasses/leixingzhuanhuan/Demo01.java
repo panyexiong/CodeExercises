385b6fc5f05cc01e4a63534fa510e1e71d11bf50
panyexiong <289573907@qq.com>
2019-08-22 22:21:43 +0800

    update

---
+++ CodeExercises-CommonClasses/src/main/java/com/pan/commonClasses/leixingzhuanhuan/Demo01.java
@@ -0,0 +1,18 @@
+package com.pan.commonClasses.leixingzhuanhuan;
+
+/**
+ * @author panyexiong
+ * @version 1.0
+ * @date 2019/8/22 21:54
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        double d1 = 12.3;
+        int i1 = (int) d1;
+        System.out.println(i1);
+
+        int i2 =128;
+        byte b1 = (byte) i2;
+        System.out.println(b1);
+    }
+}
---
+++ CodeExercises-CommonClasses/src/main/java/com/pan/commonClasses/string/Demo01.java
@@ -0,0 +1,20 @@
+package com.pan.commonClasses.string;
+
+/**
+ * @author panyexiong
+ * @version 1.0
+ * @date 2019/8/22 22:08
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        int num1 = 0b110;
+        int num2 = 110;
+        int num3 = 0127;
+        int num4 = 0x110A;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
