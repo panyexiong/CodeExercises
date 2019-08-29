9f0d7dc424e70899efcd4710ec918d54ff3f4c53
panyexiong <289573907@qq.com>
2019-08-30 00:06:24 +0800

    update

--- CodeExercises-CommonClasses/src/main/java/com/pan/commonClasses/collection/list/Demo01.java
+++ CodeExercises-CommonClasses/src/main/java/com/pan/commonClasses/collection/list/Demo01.java
@@ -1,10 +1,19 @@
 package com.pan.commonClasses.collection.list;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * @author panyexiong
  * @version 1.0
  * @date 2019/8/29 21:44
  */
 public class Demo01 {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
 
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(1);
+    }
 }
