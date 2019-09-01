eef8b76d3965209c33e9a6a997bb64aba469ab71
panyexiong <289573907@qq.com>
2019-09-01 22:36:37 +0800

    update

---
+++ CodeExercises-Thread/src/main/java/com/pan/codeExercises/enumTest/EnumTest.java
@@ -0,0 +1,10 @@
+package com.pan.codeExercises.enumTest;
+
+/**
+ * @author panyexiong
+ * @version 1.0
+ * @date 2019/9/1 22:30
+ */
+public enum EnumTest {
+
+}
--- CodeExercises-Thread/src/main/java/com/pan/codeExercises/thread/CountDownLatchDemo.java
+++ CodeExercises-Thread/src/main/java/com/pan/codeExercises/thread/CountDownLatchDemo.java
@@ -1,12 +1,27 @@
 package com.pan.codeExercises.thread;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author panyexiong
  * @version 1.0
  * @date 2019/8/28 21:59
  */
 public class CountDownLatchDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+    }
+
+    private static void test01() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+
+        for (int i = 0; i < 6; i++) {
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName());
+                countDownLatch.countDown();
+            }).start();
+        }
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName());
     }
 }
