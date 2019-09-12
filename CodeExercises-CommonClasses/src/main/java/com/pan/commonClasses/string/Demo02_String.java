f10761edf503007d3d768f291102802b58f95680
panyexiong <289573907@qq.com>
2019-09-12 19:54:07 +0800

    update

---
+++ CodeExercises-CommonClasses/src/main/java/com/pan/commonClasses/string/Demo02_String.java
@@ -0,0 +1,17 @@
+package com.pan.commonClasses.string;
+
+/**
+ * @author panyexiong
+ * @version 1.0
+ * @date 2019/9/12 17:46
+ */
+public class Demo02_String {
+    public static void main(String[] args) {
+        String string1 = "123";
+        String string2 = new String("1234");
+
+
+        StringBuilder stringBuilder1 = new StringBuilder();
+        StringBuilder stringBuilder2 = new StringBuilder("21312");
+    }
+}
