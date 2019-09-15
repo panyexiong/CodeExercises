335f6b1a6e59ac8a86cac85fb0a2f84c77ab7b3e
panyexiong <289583907@qq.com>
2019-09-15 23:56:22 +0800

    update

---
+++ CodeExercises-CommonClasses/src/main/java/com/pan/commonClasses/collection/queue/Demo01.java
@@ -0,0 +1,10 @@
+package com.pan.commonClasses.collection.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        BlockingQueue arrayBlockingQueue = new ArrayBlockingQueue(1);
+    }
+}
