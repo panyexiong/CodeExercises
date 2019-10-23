02d5eb70e98d883dbe5264ef4241f4efab364a81
panyexiong <289573907@qq.com>
2019-10-23 18:59:28 +0800

    update

---
+++ CodeExercises-BiShi/src/main/java/com/pan/mianshi/wangyihuyu/Demo01.java
@@ -0,0 +1,12 @@
+package com.pan.mianshi.wangyihuyu;
+
+/**
+ * @author panyexiong
+ * @version 1.0
+ * @date 2019/10/23 16:26
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+
+    }
+}
