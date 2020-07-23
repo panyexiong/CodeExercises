6b5632810fa9e8d0a6ee54b874de9b4f5aa09adc
panyexiong <panyexiong@xinye.com>
2020-07-23 19:27:10 +0800

    update

--- CodeExercises-DataStructure/src/main/java/com/pan/dataStructure/sort/InsertSort.java
+++ CodeExercises-DataStructure/src/main/java/com/pan/dataStructure/sort/InsertSort.java
@@ -1,11 +1,13 @@
 package com.pan.dataStructure.sort;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 public class InsertSort {
     public static void main(String[] args) {
         int[] arr = {3, -2, 6, 1, 9, 5, 2, 0, 3, 4};
-        insertSort(arr);
+        insertSort1(arr);
     }
 
     public static void insertSort(int[] arr) {
@@ -25,5 +27,26 @@ public class InsertSort {
             System.out.printf("第%d轮排序" + Arrays.toString(arr) + "\n", i - 1);
         }
         System.out.println("最终结果" + Arrays.toString(arr));
+    }
+
+    public static void insertSort1(int[] arr) {
+        System.out.println("原数组：" + Arrays.toString(arr));
+
+        for (int i = 1; i < arr.length; i++) {
+            //temp当前需要比较的值
+            Integer temp = arr[i];
+            int j;
+            for (j = i; j > 0 && temp.compareTo(arr[j - 1]) < 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = temp;
+        }
+
+        System.out.println("最终结果" + Arrays.toString(arr));
+    }
+
+    @Test
+    public void test1() {
+
     }
 }
